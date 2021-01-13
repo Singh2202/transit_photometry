06ce9459d98e950beeefcc3777a97bc4d4c11091
Singh2202 <71194920+Singh2202@users.noreply.github.com>
2021-01-12 22:13:58 -0500

    Update working_flux_classifier.py

--- working_flux_classifier.py
+++ working_flux_classifier.py
@@ -1,7 +1,9 @@
 unique_fluxes = unique_fluxes
+
+#(unique_fluxes)
 # any value in unique_fluxes is considered to be a "base case"
 # unqcomputed in the cell above = [0.9994737591114569, 0.9985959193815361]
-min_fluxes = [0.99947375911145686, 0.99859591938153613, 0.99956234254866916, 0.99865517128565569, 0.99952370911155686, 0.99866017148565545]
+
 # not sure why but min_fluxes isn't printing correctly, so here it is being declared again
 
 min_fluxes = list(set(min_fluxes) - set(unique_fluxes)) # remove the base cases in unique_fluxes from min_fluxes
@@ -22,7 +24,17 @@ list_of_classes = [[] for n in range(len(unique_fluxes))] #create list of lists 
 for index in range(len(list_of_classes)): #populate each blank list with one unqiue flux
     list_of_classes[index].append(unique_fluxes[index]) #double loop not necessary because len(nested_list) == len(unique_fluxes)
     
-for a_list in list_of_classes: #each list will correspond to one class of flux
+for a_class in list_of_classes: #each list will correspond to one class of flux
     for value in correspondence_list:
-        if value[0] in a_list: #if the base case for each flux is equal to the base case in the list
-            a_list.append(value[1]) #append to the flux class the non-base flux
+        if value[0] in a_class: #if the base case for each flux is equal to the base case in the list
+            a_class.append(value[1]) #append to the flux class the non-base flux
+
+print(tuples)
+for a_tuple in tuples:
+    for a_class in list_of_classes:
+        for element in a_class: 
+            if element == a_tuple[1]: # if a flux in the internal list is equal to the flux of a single tuple
+                a_class.append(a_tuple[0]) # append to the class list the x coord associated with that flux
+
+for index in range(len(list_of_classes)):
+    list_of_classes[index] = list_of_classes[index][len(list_of_classes[index])//2:] #split internal class lists in half so that only the x_coords remain, b/c as many x_coords as fluxes by def
